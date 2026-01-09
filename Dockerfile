a4dfa4e083994f6ddedfe5eb07548abf55f66882
gnom48 <gnom.48.spb@gmail.com>
2026-01-09 23:13:19 +0300

    run on flask.run (((

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM python:3.12-slim
+
+WORKDIR /app
+
+ARG SITE_PORT   
+ENV SITE_PORT=${SITE_PORT}  
+
+RUN apt-get update && \
+    apt-get install -y curl
+
+COPY requirements.txt ./
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY . .
+
+# ENTRYPOINT ["sh", "-c"]
+# CMD ["gunicorn", "-w", "1", "-b", "0.0.0.0:${SITE_PORT}", "main:asgi_app"]
+CMD ["python", "-m", "main"]
--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from flask import Flask, render_template, url_for
 import os
 import traceback
 from datetime import datetime
-from dotenv import load_dotenv
 from flask import Flask, request, jsonify
 from telegram import Bot
 from telegram.constants import ParseMode
 import asyncio
 
-app = Flask(__name__)
+
+asgi_app = Flask(__name__)
+
 
 def escape_markdown_v2(text):
     special_chars = r'\_*[]()~`>#+-=|{}.'
@@ -16,25 +17,28 @@ def escape_markdown_v2(text):
         text = text.replace(char, '\\' + char)
     return text
 
+
 def send_message(text):
-    load_dotenv()
-    token = '8301890141:AAG-4HhD7Doe3mzC4WOds-B0EHVxm_Zhjaw'
-    chat_id = '1115007593'
+    token = os.getenv("TG_TOKEN")
+    chat_id = os.getenv("TG_CHAT_ID")
 
     bot = Bot(token=token)
     safe_text = escape_markdown_v2(text)
-    asyncio.run(bot.send_message(chat_id=chat_id, text=safe_text, parse_mode=ParseMode.MARKDOWN_V2))
+    asyncio.run(bot.send_message(chat_id=chat_id, text=safe_text,
+                parse_mode=ParseMode.MARKDOWN_V2))
 
-@app.route("/")
+
+@asgi_app.route("/")
 def home():
     return render_template("index.html", title="DariX — IT-решения")
 
-@app.route("/privacy")
+
+@asgi_app.route("/privacy")
 def privacy():
     return render_template("privacy.html", title="DariX — IT-решения")
 
 
-@app.route('/submit_request', methods=['POST'])
+@asgi_app.route('/submit_request', methods=['POST'])
 def submit_request():
     data = request.get_json()
 
@@ -61,7 +65,7 @@ def submit_request():
         return jsonify({'result': False}), 500
 
 
-@app.route('/consultation_form', methods=['POST'])
+@asgi_app.route('/consultation_form', methods=['POST'])
 def consultation_form():
     """Обработка формы консультации"""
     data = request.get_json()
@@ -83,6 +87,12 @@ def consultation_form():
         print(traceback.format_exc())
         return jsonify({'result': False}), 500
 
+
+@asgi_app.route("/health_check")
+def health_check():
+    return jsonify({"ok": True})
+
+
 # ---------------- Run ----------------
 if __name__ == '__main__':
-    app.run(debug=True)
+    asgi_app.run(debug=False, port=os.getenv("SITE_PORT"), host="0.0.0.0")
